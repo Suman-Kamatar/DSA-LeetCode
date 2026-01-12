36bbe1fe45f577ce0c5d783d561b5bbbdc4d3834
Suman-Kamatar <kamatarsuman12@gmail.com>
2026-01-12 07:49:47 +0530

    Added Leetcode #14 Longest Common Prefix

---
+++ Array/Longest_Common_Prefix.py
@@ -0,0 +1,22 @@
+#14 Longest Common Prefix
+#Difficulty - Easy
+#Topic - Array and String
+#Time Complexity - O(n)
+#Space Complexity - O(1)
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if not strs:
+            return ""
+        
+        prefix = strs[0]
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
+
+#Example Usage
+sol = Solution()    
+print(sol.longestCommonPrefix(["flower","flow","flight"]))  #Output: "fl"
