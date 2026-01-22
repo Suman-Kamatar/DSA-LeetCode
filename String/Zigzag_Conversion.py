7436c343a3af69b4b99e5e8d34326fb93a72b352
Suman-Kamatar <kamatarsuman12@gmail.com>
2026-01-22 20:05:17 +0530

    Added Leetcode String - Medium

---
+++ String/Zigzag_Conversion.py
@@ -0,0 +1,28 @@
+#6 Zigzag Conversion
+#Difficulty - Medium
+#Topics = String 
+#Time Complexity - O(n)
+#Space Complexity - O(n)
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        index, step = 0, 1
+
+        for char in s:
+            rows[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return ''.join(rows)
+
+# Example usage:
+solution = Solution()
+result = solution.convert("PAYPALISHIRING", 3)
+print(result)  # Output: "PAHNAPLSIIGYIR"
